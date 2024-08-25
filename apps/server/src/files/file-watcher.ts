a8760a9904dbbabe133844190e340ef7e2589865
Philip Andrew Wee De Wang <philip@firejet.io>
2024-08-25 12:32:59 +0800

    improved file watching

--- apps/server/src/files/file-watcher.ts
+++ apps/server/src/files/file-watcher.ts
@@ -3,11 +3,12 @@ import { getWorkingDir } from '../helpers/get-working-dir';
 import { getGitIgnoredFiles } from './get-ignore-patterns';
 
 const workingDir = getWorkingDir();
+console.log(getGitIgnoredFiles(workingDir));
 const watcher = chokidar.watch(workingDir, {
   ignored: getGitIgnoredFiles(workingDir),
 });
-'bu'
+
 watcher.on('ready', () => {
-//   const watchedPaths = watcher.gedtWatched(); 
-  console.log(getGitIgnoredFiles(workingDir));
+  const watchedPaths = watcher.getWatched();
+  console.log(watchedPaths);
 });
--- apps/server/src/files/get-ignore-patterns.ts
+++ apps/server/src/files/get-ignore-patterns.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import parse from 'parse-gitignore';
 
 export function getGitIgnoredFiles(dir: string): string[] {
   const gitignorePath = path.join(dir, '.gitignore');
@@ -7,9 +8,21 @@ export function getGitIgnoredFiles(dir: string): string[] {
     return [];
   }
 
-  const content = fs.readFileSync(gitignorePath, 'utf8');
-  return content
-    .split('\n')
-    .filter((line) => line.trim() !== '' && !line.startsWith('#'));
-}
+  //TODO: Fix types
+  const parsed = parse(fs.readFileSync(gitignorePath, 'utf8')) as any;
 
+  const ignoredGlobs: string[] = parsed.globs().flatMap((val: any) => {
+    if (val.type !== 'ignore') return [];
+    return val.patterns as string[];
+  });
+
+  ignoredGlobs.push('.git/**');
+
+  const additionalGlobs = ignoredGlobs
+    .filter((glob) => glob.startsWith('.') && !glob.startsWith('./'))
+    .map((glob) => `**/${glob}`);
+
+  ignoredGlobs.push(...additionalGlobs);
+
+  return ignoredGlobs;
+}
