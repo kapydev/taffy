18fbbf2216755c87a3a7395fad4f0c91521d57b8
Philip Andrew Wee De Wang <philip@firejet.io>
2024-09-20 13:51:45 +0800

    Made web view

--- apps/vsc-ext/src/main.ts
+++ apps/vsc-ext/src/main.ts
@@ -1,25 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-console.log("dinwqondwqidnwoq")
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand('cto-ai.start', () => {
+      // Create and show a new webview
+      const panel = vscode.window.createWebviewPanel(
+        'catCoding', // Identifies the type of the webview. Used internally
+        'CTO AI', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('cto-ai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	  panel.webview.html = '<div>test<button>tindiwnodqw</button></div>'
+    })
+  );
 }
