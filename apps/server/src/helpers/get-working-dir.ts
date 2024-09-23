e4c469be9dbf19ee5efb170f6ecb3987e913ccdf
Philip Andrew Wee De Wang <philip@firejet.io>
2024-09-23 15:09:34 +0800

    removed server

--- apps/server/src/helpers/get-working-dir.ts
+++
@@ -1,3 +0,0 @@
-export function getWorkingDir() {
-  return process.cwd();
-}
--- apps/server/src/types.ts
+++
@@ -1,6 +0,0 @@
-// Export type router type signature,
-
-import { appRouter } from './main';
-
-// NOT the router itself.
-export type AppRouter = typeof appRouter;
