2836ebf58413996e8959f9600b37176493da1d40
Philip Andrew Wee De Wang <philip@firejet.io>
2024-09-25 11:07:58 +0800

    Now able to remove codebase context if not needed

--- apps/cto-ai/src/app/Messages/FileContextMessageRender.tsx
+++ apps/cto-ai/src/app/Messages/FileContextMessageRender.tsx
@@ -1,13 +1,22 @@
 import { Alert, AlertDescription, AlertTitle } from '@cto-ai/components';
 import { FilePlus2Icon } from 'lucide-react';
 import { FileContextMessage } from '../../llms/messages/FileContextMessage';
-import { useMemo } from 'react';
+import { useMemo, useState, useCallback } from 'react';
+import { chatStore } from '../../stores/chat-store';
+import { Button } from 'react-day-picker';
 
 export function FileContextMessageRender({
   message,
 }: {
   message: FileContextMessage;
 }) {
+  const removeMessage = useCallback(() => {
+    chatStore.set(
+      'messages',
+      chatStore.get('messages').filter((msg) => msg !== message)
+    );
+  }, [message]);
+
   const title = useMemo(() => {
     if (message.file?.content) {
       return 'The following file have been added to the context';
@@ -22,6 +31,11 @@ export function FileContextMessageRender({
       <FilePlus2Icon className="w-4 h-4" />
       <AlertTitle>{title}</AlertTitle>
       <AlertDescription>{message.filePath}</AlertDescription>
+      <div className="flex gap-2">
+        <Button className="text-vsc-errorForeground" onClick={removeMessage}>
+          Remove
+        </Button>
+      </div>
     </Alert>
   );
 }
