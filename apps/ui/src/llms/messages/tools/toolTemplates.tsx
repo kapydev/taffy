84e9b8873ca903645020028e0304b6185edf882d
Philip Andrew Wee De Wang <philip@firejet.io>
2024-10-08 09:18:30 +0800

    Initial prompt updates

--- apps/ui/src/llms/messages/tools/toolTemplates.tsx
+++ apps/ui/src/llms/messages/tools/toolTemplates.tsx
@@ -1,4 +1,10 @@
-import { BotIcon, FilePlus2Icon, LucideProps, UserIcon } from 'lucide-react';
+import {
+  BotIcon,
+  FilePlus2Icon,
+  LucideProps,
+  UserIcon,
+  WaypointsIcon,
+} from 'lucide-react';
 import { trpc } from '../../../client';
 import { ToolMessage } from '../ToolMessage';
 
@@ -25,55 +31,49 @@ export interface ToolTemplate {
   sampleProps: Record<string, string>;
 }
 
+/**
+ * Prompt Engineering Guidelines:
+ * CoT - Ask LLM to explain thought process step by step for better outputs
+ * Citations - By asking LLM for citation, we can run a bullshit detector using regex
+ * No negatives - Instead of saying `DO NOT DO XXX`, which places a focus on XXX cos that's how attention models work, say `DO YYY`
+ * Avoid controlling termination behaviour - the models can't control themselves if you ask them `STOP GENERATING AFTER YYY`. Use stop sequences instead, or if you use xml tags, with rules, they can recognise that as a reason to stop
+ * Common terminology - Instead of using uncommon terminology like 'planning block' and 'fence blocks', stick to what is common for the LLM to have a better understanding of the rules, like 'thinking blocks' and 'xml tags'
+ *
+ * Prompt Engineering Ideas:
+ * Switch from fence blocks to XML tags <taffythinking> for example
+ * Make language positive instead of negative
+ * Fail fallback to repeat prompt if output is not parsable
+ * Rename planning block to thinking block
+ * Add more files in folder to context automatically
+ * Add diagnositcs to context automatically
+ * Add typescript types to context automatically - To what depth?
+ * Add token limit registrar to context adder
+ * Explicitly state indentation for replace block flow
+ * Fix assistant read file
+ */
+
 export const TOOL_TEMPLATES = {
   //ASSISTANT TOOLS
   ASSISTANT_INFO: {
     role: 'assistant',
-    desc: 'For the assistant to write a response to the user.',
+    desc: 'For the assistant to write a response to the user. Every response to the user should start with an assistant info block.',
     propDesc: {},
     sampleProps: {},
     sampleBody:
-      'To prevent .env files from being committed into the codebase, we need to update the .gitignore file',
+      'To prevent .env files from being committed into the codebase, we need to update the .gitignore file.',
   },
-  // TODO: For multi file, replace assistant planning with thinking blocks
-  // ASSISTANT_PLANNING: {
-  //   role: 'assistant',
-  //   desc: "For the assistant to plan how to tackle the task from the user. There should be a planning block after every assistant info tool block. Reason how to tackle the user's task step by step, placing steps in a logical order. After the planning tool is done, execute the plan. In each step, indicate what tool you will use, and how you will use it",
-  //   propDesc: {},
-  //   sampleProps: {},
-  //   sampleBody: `Example 1:
-  //   1. Read the .gitignore file using ASSISTANT_READ_FILE
-  //   2. Update the .gitignore file using ASSISTANT_WRITE_FILE`,
-  // },
-  //   ASSISTANT_WRITE_FILE: {
-  //     role: 'assistant',
-  //     desc: `Ask the user for permission to create/overwrite a file. You will need to provide the FULL FILE CONTENTS, because the action suggested to the user will be a full override of the existing file. DO NOT INCLUDE LINE NUMBERS IN THE OUTPUT.`,
-  //     propDesc: {
-  //       filePath:
-  //         "The path to which the file is written. If the file path doesn't exist, directories will be recursively created until we are able to create the file.",
-  //     },
-  //     sampleProps: {
-  //       filePath: 'src/utils/helloWorld.ts',
-  //     },
-  //     sampleBody: `export function helloWorld() {
-  // ${'console'}.log("Hello World!");
-  // }`,
-  //   },
-  //I'm not sure about the thinking start version
+  ASSISTANT_PLANNING: {
+    role: 'assistant',
+    desc: "For the assistant to plan how to tackle the task from the user. If an assistant info block or the user's prompt suggests that the user's codebase will be edited, the planning block should be between the info block and the write file block, or whatever action on the users codebase. Reason how to tackle the user's task step by step, placing steps in a logical order. After the planning tool is done, execute the plan. In each step, indicate what tool you will use, and how you will use it",
+    propDesc: {},
+    sampleProps: {},
+    sampleBody: `Example 1:
+    1. Update the src/utils/helloWorld.ts file using ASSISTANT_WRITE_FILE
+    2. Update the barrel file src/utils/index.ts to include the export from helloWorld.ts using ASSISTANT_WRITE_FILE`,
+  },
   ASSISTANT_WRITE_FILE: {
     role: 'assistant',
-    desc: `Ask the user for permission to create/overwrite a file. You will need to provide the FULL FILE CONTENTS, because the action suggested to the user will be a full override of the existing file. Stopping generation before reaching the end of the file will result in a confusing output to the end user after the result is parsed. DO NOT INCLUDE LINE NUMBERS IN THE OUTPUT. Before tackling a challenging part of the code, you can walk yourself through the coding process in a THINKING block
-
-  {THINKING_START}
-  In order to write this function, I will need to...
-  {THINKING_END}
-
-  The thinking blocks will not be included in the outputted code.
-  Within the thinking blocks, consider the user's existing code style and practices and follow those.
-  DO NOT WRITE CODE WITHIN THE THINKING BLOCKS. The thinking blocks are soley meant for planning. Any code will need to be written outside the thinking block, in order to be included in the final suggestion to the user.
-
-  Do not generate comments in the generated code, unless the user explicitly asks for it. Use thinking blocks instead if absolutely necessary.
-  `,
+    desc: `Ask the user for permission to create/overwrite a file. You will need to provide the FULL FILE CONTENTS, because the action suggested to the user will be a full override of the existing file.`,
     propDesc: {
       filePath:
         "The path to which the file is written. If the file path doesn't exist, directories will be recursively created until we are able to create the file.",
@@ -82,19 +82,27 @@ export const TOOL_TEMPLATES = {
       filePath: 'src/utils/helloWorld.ts',
     },
     sampleBody: `export function helloWorld() {
-{THINKING_START}
-The hello world function should log hello world.
-{THINKING_END}
   ${'console'}.log("Hello World!");
 }`,
   },
-  ASSISTANT_READ_FILE: {
+  ASSISTANT_REPLACE_BLOCK: {
     role: 'assistant',
-    desc: "Ask the user for permission to add a file to the context. The contents of the tool call should be all the files that need to be read, seperated by newlines. After calling this tool, no other tool calls can be made by the assistant, as we have to wait for the user's response",
-    propDesc: {},
-    sampleProps: {},
-    sampleBody: 'src/index.ts\nsrc/messages/helloWorld.ts',
+    desc: `A block from the assistant to address a user's focus block. You will need to provide ONLY the contents of the code to replace the block. The focus block will be the only part of the code programmatically replaced with the replace block, so make sure the outputs are immediately runnable. Instead of including comments in the output of the replace block, any parts of the thought process should be in the planning block or info block instead. A replace block should only address the latest USER_FOCUS_BLOCK in the prompt history. If there is more than one USER_FOCUS_BLOCK in the prompt history, the rest can be ignored.`,
+    propDesc: {
+      filePath: 'The file path where the contents are from',
+    },
+    sampleProps: {
+      filePath: 'src/utils/helloWorld.ts',
+    },
+    sampleBody: `  const name = 'Robert';`,
   },
+  // ASSISTANT_READ_FILE: {
+  //   role: 'assistant',
+  //   desc: "Ask the user for permission to add a file to the context. The contents of the tool call should be all the files that need to be read, seperated by newlines. After calling this tool, no other tool calls can be made by the assistant, as we have to wait for the user's response",
+  //   propDesc: {},
+  //   sampleProps: {},
+  //   sampleBody: 'src/index.ts\nsrc/messages/helloWorld.ts',
+  // },
   //USER TOOLS
   USER_PROMPT: {
     role: 'user',
@@ -105,23 +113,32 @@ The hello world function should log hello world.
   },
   USER_FILE_CONTENTS: {
     role: 'user',
-    desc: 'Information from the user regarding the contents of a file. If there are multiple FILE_CONTENTS tool responses, the latest one should be considered as the correct one. Questions from the user should be focused on parts of the code within a FOCUS fence block ',
+    desc: 'Information from the user regarding the contents of a file. If there are multiple FILE_CONTENTS tool responses, the latest one should be considered as the correct one.',
     propDesc: {
-      startLine: 'The start line of the specific area to focus on',
-      endLine: 'The end line of the specific area to focus on',
       filePath: 'The file path where the contents are from',
     },
     sampleProps: {
-      startLine: '1',
-      endLine: '25',
       filePath: 'src/utils/helloWorld.ts',
     },
     sampleBody: `export default function HelloWorld() {
-{FOCUS_START}
-    const name = 'Thomas';
-{FOCUS_END}
-    ${'console'}.log("Hello World");
-  }`,
+  const name = 'Thomas';
+  ${'console'}.log("Hello World");
+}`,
+  },
+  USER_FOCUS_BLOCK: {
+    role: 'user',
+    desc: 'A block from the user that specifies a particular section of the code that he has a question about, or he wants to replace. If only a question is asked, just reply the question in the info block. If the user wants the code to be edited, run through the info -> planning -> replace block flow. Make sure to match the original indendation of the line.',
+    propDesc: {
+      filePath: 'The file path where the contents are from',
+      startLine: 'The start line of the specific area to focus on',
+      endLine: 'The end line of the specific area to focus on',
+    },
+    sampleProps: {
+      filePath: 'src/utils/helloWorld.ts',
+      startLine: '2',
+      endLine: '2',
+    },
+    sampleBody: `  const name = 'Thomas';`,
   },
   USER_AVAILABLE_FILES: {
     role: 'user',
@@ -165,26 +182,26 @@ export const TOOL_RENDER_TEMPLATES: {
     title: () => 'Assistant Info',
     description: (data) => data.body,
   },
-  // ASSISTANT_PLANNING: {
-  //   Icon: WaypointsIcon,
-  //   title: () => 'Assistant Planning',
-  //   description: (data) => data.body,
-  // },
-  ASSISTANT_READ_FILE: {
-    Icon: FilePlus2Icon,
-    title: () => 'Requesting permission to read the following files',
+  ASSISTANT_PLANNING: {
+    Icon: WaypointsIcon,
+    title: () => 'Assistant Planning',
     description: (data) => data.body,
-    actions: [
-      {
-        name: 'approve',
-        action: () => {},
-        shortcutEnd: 'enter',
-      },
-    ],
   },
-  USER_FILE_CONTENTS: {
-    Icon: FilePlus2Icon,
-    title: () => 'File Context Added',
+  // ASSISTANT_READ_FILE: {
+  //   Icon: FilePlus2Icon,
+  //   title: () => 'Requesting permission to read the following files',
+  //   description: (data) => data.body,
+  //   actions: [
+  //     {
+  //       name: 'approve',
+  //       action: () => {},
+  //       shortcutEnd: 'enter',
+  //     },
+  //   ],
+  // },
+  USER_FOCUS_BLOCK: {
+    Icon: UserIcon,
+    title: () => 'User Focus Block',
     description: (data) => {
       if (!data.props) return;
       return (
@@ -193,6 +210,21 @@ export const TOOL_RENDER_TEMPLATES: {
           {data.props.endLine}
         </>
       );
+    },
+  },
+  ASSISTANT_REPLACE_BLOCK: {
+    Icon: FilePlus2Icon,
+    title: () => 'Requesting permission to write the following files',
+    description: (data) => {
+      return data.body;
+    },
+  },
+  USER_FILE_CONTENTS: {
+    Icon: FilePlus2Icon,
+    title: () => 'File Context Added',
+    description: (data) => {
+      if (!data.props) return;
+      return data.props.filePath;
     },
   },
   ASSISTANT_WRITE_FILE: {
@@ -257,3 +289,33 @@ export const TOOL_RENDER_TEMPLATES: {
     },
   },
 };
+
+// //I'm not sure about the thinking start version
+// ASSISTANT_WRITE_FILE: {
+//   role: 'assistant',
+//   desc: `Ask the user for permission to create/overwrite a file. You will need to provide the FULL FILE CONTENTS, because the action suggested to the user will be a full override of the existing file. Stopping generation before reaching the end of the file will result in a confusing output to the end user after the result is parsed. DO NOT INCLUDE LINE NUMBERS IN THE OUTPUT. Before tackling a challenging part of the code, you can walk yourself through the coding process in a THINKING block
+
+// {THINKING_START}
+// In order to write this function, I will need to...
+// {THINKING_END}
+
+// The thinking blocks will not be included in the outputted code.
+// Within the thinking blocks, consider the user's existing code style and practices and follow those.
+// DO NOT WRITE CODE WITHIN THE THINKING BLOCKS. The thinking blocks are soley meant for planning. Any code will need to be written outside the thinking block, in order to be included in the final suggestion to the user.
+
+// Do not generate comments in the generated code, unless the user explicitly asks for it. Use thinking blocks instead if absolutely necessary.
+// `,
+//   propDesc: {
+//     filePath:
+//       "The path to which the file is written. If the file path doesn't exist, directories will be recursively created until we are able to create the file.",
+//   },
+//   sampleProps: {
+//     filePath: 'src/utils/helloWorld.ts',
+//   },
+//   sampleBody: `export function helloWorld() {
+// {THINKING_START}
+// The hello world function should log hello world.
+// {THINKING_END}
+// ${'console'}.log("Hello World!");
+// }`,
+// },
